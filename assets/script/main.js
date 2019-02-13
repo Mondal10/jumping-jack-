c3525f11d1c2124c786fd6a0cac65c28cbd52cc8
Mondal10 <amitmondal111.am@gmail.com>
2019-02-13 19:02:11 +0530

    update

--- assets/script/main.js
+++ assets/script/main.js
@@ -31,7 +31,7 @@ function init(){
         this.add.image(0, 0, 'sky').setOrigin(0, 0); //or use image(400, 300, 'sky') without setorigin
        
         platforms = this.physics.add.staticGroup();
-        platforms.create(400, 568, 'platform').setScale(2)/* .refreshBody() */;
+        platforms.create(400, 568, 'platform').setScale(2).refreshBody(); // The call to refreshBody() is required because we have scaled a static physics body, so we have to tell the physics world about the changes we made.
         platforms.create(600, 400, 'platform');
         platforms.create(50, 250, 'platform');
         platforms.create(750, 220, 'platform');
