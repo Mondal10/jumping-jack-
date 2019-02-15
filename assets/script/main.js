b9e6b68ffcaa1307cc714838016180a986f51241
Mondal10 <amitmondal111.am@gmail.com>
2019-02-15 22:35:06 +0530

    beautify

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,13 +1,13 @@
-function init(){
+function init() {
     var platforms;
     // configuration to create scene
     var config = {
-        type: Phaser.AUTO, 
+        type: Phaser.AUTO,
         width: 800,
         height: 600,
         physics: {
             default: 'arcade',
-            gravity: {y: 300},
+            gravity: { y: 300 },
             debug: false
         },
         scene: {
@@ -17,34 +17,31 @@ function init(){
         }
     };
 
-    function preload ()
-    {
-        this.load.image('sky','../../assets/images/sky.png');
-        this.load.image('platform','../../assets/images/platform.png');
-        this.load.image('star','../../assets/images/star.png');
-        this.load.image('bomb','../../assets/images/bomb.png');
-        this.load.spritesheet('jack','../../assets/images/jack.png',{ frameWidth: 32, frameHeight: 48 });
+    function preload() {
+        this.load.image('sky', '../../assets/images/sky.png');
+        this.load.image('platform', '../../assets/images/platform.png');
+        this.load.image('star', '../../assets/images/star.png');
+        this.load.image('bomb', '../../assets/images/bomb.png');
+        this.load.spritesheet('jack', '../../assets/images/jack.png', { frameWidth: 32, frameHeight: 48 });
     }
-    
-    function create ()
-    {
+
+    function create() {
         this.add.image(0, 0, 'sky').setOrigin(0, 0); //or use image(400, 300, 'sky') without setorigin
-       
+
         platforms = this.physics.add.staticGroup();
         platforms.create(400, 568, 'platform').setScale(2).refreshBody(); // The call to refreshBody() is required because we have scaled a static physics body, so we have to tell the physics world about the changes we made.
         platforms.create(600, 400, 'platform');
         platforms.create(50, 250, 'platform');
         platforms.create(750, 220, 'platform');
     }
-    
-    function update ()
-    {
+
+    function update() {
     }
 
     var game = new Phaser.Game(config);
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     // $div = $('<div></div>').addClass('asdasd').appendTo('body');
     // alert("window is loaded");
     init();
