34b8447b59e349e8991ec4f7ee3567e88d2a69f3
Mondal10 <amitmondal111.am@gmail.com>
2019-02-15 23:36:14 +0530

    creating jack

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,5 +1,6 @@
 function init() {
     var platforms;
+    var player;
     // configuration to create scene
     var config = {
         type: Phaser.AUTO,
@@ -26,13 +27,41 @@ function init() {
     }
 
     function create() {
+        //Sky background
         this.add.image(0, 0, 'sky').setOrigin(0, 0); //or use image(400, 300, 'sky') without setorigin
+        // this.add.image(400, 300, 'star');
 
+        //Platform
         platforms = this.physics.add.staticGroup();
         platforms.create(400, 568, 'platform').setScale(2).refreshBody(); // The call to refreshBody() is required because we have scaled a static physics body, so we have to tell the physics world about the changes we made.
         platforms.create(600, 400, 'platform');
         platforms.create(50, 250, 'platform');
         platforms.create(750, 220, 'platform');
+
+        //Jack
+        player = this.physics.add.sprite(100, 500, 'jack');
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('jack', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{ key: 'dude', frame: 4 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('jack', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
     }
 
     function update() {
